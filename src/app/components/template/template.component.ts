be53691a6e597a7b3df304efdaddf22c8560ea48
vsanchezrod <v.sanchez.rod@gmail.com>
2018-07-31 10:40:05 +0200

    Validaciones y clases bootstrap para mostrar errores y mensajes

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,13 +4,14 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: [`
-    .ng-invalid.ng-touched:not(form) {
+    /*.ng-invalid.ng-touched:not(form) {
       border: 1px solid red;
-    }
+    }*/
 
   `]
 })
