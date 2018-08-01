fc319ed00f62dc6a6eb40f597b678c709803ab58
vsanchezrod <v.sanchez.rod@gmail.com>
2018-08-01 12:03:57 +0200

    Validaciones personalizadas. Validación de passwords iguales

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 // Importaciones básicas para trabajar con la aproximación por data
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -18,7 +18,8 @@ export class DataComponent {
       nombre: 'Virginia',
       apellido: 'Sánchez'
     },
-    email: 'v.sanchez.rod@gmail.com'
+    email: 'v.sanchez.rod@gmail.com',
+    pasatiempos: ['Correr']
   };
 
   constructor() {
@@ -29,14 +30,23 @@ export class DataComponent {
 
       'nombreCompleto': new FormGroup({
         'nombre': new FormControl('', [Validators.required, Validators.minLength(5)]),
-        'apellido': new FormControl('', Validators.required)
+        'apellido': new FormControl('', [Validators.required, this.noSanchez])
       }),
-      'email': new FormControl('' , [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      'email': new FormControl('' , [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl('')
+
     });
 
-    // Para cargar los datos de un usuario en el formulario
-    // El objeto a cargar debe tener la misma estructura que la forma
-    this.forma.setValue(this.usuario);
+    // Para cargar los datos de un usuario en el formulario. Eobjeto a cargar debe tener la misma estructura que la forma
+    // this.forma.setValue(this.usuario);
+
+    // Validación del password
+    // Cuando se ejecutar la función, está en otro contexto y el this no existe x eso es necesario usar el bind para decirle q será this
+    this.forma.controls['password2'].setValidators([Validators.required, this.validarPassword.bind(this.forma)]);
 
   }
 
@@ -49,14 +59,58 @@ export class DataComponent {
     // this.forma.reset(this.usuario);
 
     // 2ºForma - Resetea a vacío
-    this.forma.reset({
+    /*this.forma.reset({
       nombreCompleto: {
         nombre: '',
         apellido: ''
       },
       email: ''
-    });
+    });*/
 
   }
+
+  agregarPasatiempo() {
+
+    // Con <FormArray> se le dice a Typescript q es un array y así puede usar los métodos de los arrays.
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    );
+  }
+
+
+  // VALIDACIONES: Lo ideal es tener un archivo de validaciones aparte
+
+  // Ejemplo de validacion para que no pueda ser Sanchez el apellido
+  noSanchez(control: FormControl): {[s: string]: boolean} {
+
+    if (control.value === 'sanchez') {
+      // Si devuelve algo, es que la validación falla
+      return {
+        noSanchez: true
+      };
+    }
+
+    // Si no devuelve nada, la validación no falla
+    return null;
+
+  }
+
+  // Validación passwords
+  validarPassword(control: FormControl): {[s: string]: boolean} {
+
+    // El this aquí es igual a this.forma (por el uso de bind())
+    let forma: any = this;
+
+    // Se va a colocar en el segundo control de password
+    if (control.value !== forma.controls['password1'].value) {
+      // Si devuelve algo, es que la validación falla
+      return {
+        passwordDistintos: true
+      };
+    }
+    return null;
+  }
+
+
 
 }
