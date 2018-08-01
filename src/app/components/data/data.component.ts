05f32921c0ca0ad1f48b20d360f093dac492ac8f
vsanchezrod <v.sanchez.rod@gmail.com>
2018-08-01 09:50:01 +0200

    Cargando datos en el formulario y reseteando los campos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -13,7 +13,7 @@ export class DataComponent {
   // Responsable del formulario de tipo FormGroup
   forma: FormGroup;
 
-  usuario: Object = {
+  usuario: any = {
     nombreCompleto: {
       nombre: 'Virginia',
       apellido: 'Sánchez'
@@ -31,13 +31,31 @@ export class DataComponent {
         'nombre': new FormControl('', [Validators.required, Validators.minLength(5)]),
         'apellido': new FormControl('', Validators.required)
       }),
-      'email': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      'email': new FormControl('' , [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
+
+    // Para cargar los datos de un usuario en el formulario
+    // El objeto a cargar debe tener la misma estructura que la forma
+    this.forma.setValue(this.usuario);
+
   }
 
   guardarCambios() {
     console.log('FORMA', this.forma);
     console.log('VALUES', this.forma.value);
+
+    // Para resetear los cambios y volver a tener la clase ng-pristine
+    // 1ºForma - Resetea a los valores por defecto del objeto usuario
+    // this.forma.reset(this.usuario);
+
+    // 2ºForma - Resetea a vacío
+    this.forma.reset({
+      nombreCompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      email: ''
+    });
 
   }
 
